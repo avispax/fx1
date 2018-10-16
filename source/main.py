993ca72afd452d3ce2fa66124641b8e9e6805662
omiya <mi_la_n@yahoo.co.jp>
2018-10-16 11:12:03 +0900

    20181016_am

--- source/main.py
+++ source/main.py
@@ -7,13 +7,12 @@ import myInd as mi
 
 csvfile = 'data/DAT_ASCII_USDJPY_M1_2017.csv'
 
-df0 = pd.read_csv(csvfile, sep=';',
+df = pd.read_csv(csvfile, sep=';',
                      names=('Time','Open','High','Low','Close', ''),
                      index_col='Time', parse_dates=True)
 
-df0.index += pd.offsets.Hour(7) #7時間のオフセット
+df.index += pd.offsets.Hour(7) #7時間のオフセット
 
-#df = ind.TF_ohlc(df0, 'H') #1時間足データの作成
-df = df0
+#df = ind.TF_ohlc(df, 'H') #1時間足データの作成
 
 mi.revTry(df)
